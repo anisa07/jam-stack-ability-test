2afbbce3cee30545c4e27f33391b163edb02ae55
anisa07 <askany07@gmail.com>
2020-12-19 14:38:12 +0100

    add localization

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,10 +1,22 @@
 const axios = require('axios');
+const countries = require("./countries.json");
+
+async function getNews(country) {
+    try {
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&category=business&apiKey=${process.env.NEWS_API_KEY}`);
+        return {
+            country,
+            articles: response.data.articles
+        }
+    } catch (e) {
+        console.log("e", e)
+    }
+}
 
 module.exports = async function() {
-    try {
-        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.NEWS_API_KEY}`);
-        return response.data;
-    } catch (e) {
-
-    }
+    const newsPromises = countries.map(getNews);
+    // console.log(newsPromises);
+    const promiseResult = await Promise.all(newsPromises);
+    /// console.log(promiseResult);
+    return promiseResult;
 }
