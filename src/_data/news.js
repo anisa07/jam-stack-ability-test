446843a2fdd0aca2e56f69b73a0d81815bbdef77
anisa07 <askany07@gmail.com>
2020-12-26 13:04:24 +0100

    add weather api

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ const countries = require("./countries.json");
 
 async function getNews(country) {
     try {
-        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&category=business&apiKey=${process.env.NEWS_API_KEY}?pageSize=3`);
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=3`);
         return {
             country,
             articles: response.data.articles || []
